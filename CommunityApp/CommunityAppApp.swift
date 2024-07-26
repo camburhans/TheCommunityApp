d9be07a0b7987da4bf1689454beb7f1859a4cae8
Cam Burhans <cburhans@email.unc.edu>
2024-07-26 03:09:06 -0400

    Initial Commit

---
+++ CommunityApp/CommunityAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CommunityAppApp.swift
+//  CommunityApp
+//
+//  Created by Cameron Burhans on 7/26/24.
+//
+
+import SwiftUI
+
+@main
+struct CommunityAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CommunityApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  CommunityApp
+//
+//  Created by Cameron Burhans on 7/26/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
