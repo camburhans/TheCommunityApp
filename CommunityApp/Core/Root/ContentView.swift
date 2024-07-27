dd731e755d3d0b10315b053e116609b8568c4567
Cam Burhans <cburhans@email.unc.edu>
2024-07-27 03:16:58 -0400

    Completed user-auth tutorial. Can now sign in with firebase database as the storage.

---
+++ CommunityApp/Core/Root/ContentView.swift
@@ -0,0 +1,24 @@
+//
+// ContentView.swift
+// CommunityApp
+//
+// Created by Cameron Burhans on 7/27/24
+
+import SwiftUI
+
+struct ContentView: View {
+    @EnvironmentObject var viewModel: AuthViewModel
+    var body: some View {
+        Group {
+            if viewModel.userSession != nil {
+                ProfileView()
+            } else {
+                LoginView()
+            }
+        }
+    }
+}
+
+#Preview {
+    ContentView()
+}
--- Pods/Target Support Files/Pods-CommunityApp/Pods-CommunityApp-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_CommunityApp : NSObject
-@end
-@implementation PodsDummy_Pods_CommunityApp
-@end
